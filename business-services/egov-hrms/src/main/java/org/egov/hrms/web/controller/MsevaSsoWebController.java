ce458a141287f296e78ce977f286875ea4e21075
mohdrazacs <88527057+mohdrazacs@users.noreply.github.com>
2024-12-23 11:32:33 +0530

    Change hemendra sso string response to JSON

--- business-services/egov-hrms/src/main/java/org/egov/hrms/web/controller/MsevaSsoWebController.java
+++ business-services/egov-hrms/src/main/java/org/egov/hrms/web/controller/MsevaSsoWebController.java
@@ -1,9 +1,12 @@
 package org.egov.hrms.web.controller;
 
+import java.util.Map;
+
 import org.egov.hrms.service.MsevaSsoService;
+import org.egov.hrms.web.contract.ApiResponse;
 import org.egov.hrms.web.contract.AuthenticateUserInputRequest;
+import org.egov.hrms.web.contract.SsoResponce;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,7 +24,40 @@ public class MsevaSsoWebController {
     
     
     @PostMapping("/authenticate-user")
-    public String generateSSOUrl(@RequestBody AuthenticateUserInputRequest authenticateUserInputRequest) {
-        return msevaSsoService.generateSsoUrl(authenticateUserInputRequest);
-    }
+    public ApiResponse<?> generateSSOUrl1(@RequestBody AuthenticateUserInputRequest authenticateUserInputRequest) {
+            
+        Map<String, Object> map = msevaSsoService.fetchGenerateSsoUrlDetails(authenticateUserInputRequest);
+            if (map.get("code").equals(0)) {
+
+                return new ApiResponse<>(false, (String)map.get("message")
+                    );
+            }
+            else if (map.get("code").equals(2)) {
+
+                return new ApiResponse<>(false, (String)map.get("message")
+                    );
+            }
+            else if (map.get("code").equals(3)) {
+
+                return new ApiResponse<>(false, (String)map.get("message")
+                    );
+            }
+            else if (map.get("code").equals(4)) {
+
+                return new ApiResponse<>(false, (String)map.get("message")
+                    );
+            }
+            else if (map.get("code").equals(5)) {
+                SsoResponce ssoResponce = new SsoResponce();
+                ssoResponce.setUserName((String) map.get("userName"));
+			    ssoResponce.setTenantId((String)map.get("tenantId"));
+				ssoResponce.setEmployeeType((String)map.get("employeeType"));
+				ssoResponce.setUrl((String) map.get("url"));
+                return new ApiResponse<>(true, (String)map.get("message"),ssoResponce
+                    );
+            }else{
+                return new ApiResponse<>(false, "fetch unsuccessfully",""
+                    );
+            } 
+    } 
 }
